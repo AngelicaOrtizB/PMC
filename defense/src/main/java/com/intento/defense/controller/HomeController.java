e5ff9b29bde07dddb7594542ec10504738b3904c
AngelicaOrtizB <123736661+AngelicaOrtizB@users.noreply.github.com>
2024-11-03 11:51:42 -0500

    Add files via upload

---
+++ defense/src/main/java/com/intento/defense/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.intento.defense.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/sobre")
+    public String sobre() {
+        return "sobre";
+    }
+}
+
+
